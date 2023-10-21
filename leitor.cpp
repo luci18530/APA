c3405e12284cab2fc54d6d210c75ac82d88af6c7
bigpedrolucas <p.lucasilva@hotmail.com>
2023-10-21 17:39:49 -0300

    # implementado vnd simples para teste

--- leitor.cpp
+++ leitor.cpp
@@ -12,6 +12,12 @@ vector<vector<int>> custoRotas;
 int calculaCustoRota(vector<int> &rota)
 {
     // CUSTO GLOBAL -> SUM(custo_rota) + custo_caminhoes + custo_terceirizacoes
+
+    // custo_rota = [35, 61, 27, 66, 75]
+    // custo_terceirizacoes = [15, 14, 32, 22]
+    // custo_caminhoes = custo_caminhao*quantidade_veiculos
+    // Função para aplicar a melhoria 2-opt a uma rota
+
     int custo = 0;
     int deposito = rota[0];
     int ultimo_destino = rota.back();
@@ -33,6 +39,41 @@ void swap(vector<int> &rota, int i, int j)
     int aux = rota[i];
     rota[i] = rota[j];
     rota[j] = aux;
+}
+
+vector<int> vnd(vector<int> &rota)
+{
+    // Abordagem 2-Opt
+    vector<int> rotaMelhorada = rota;
+    bool melhorRota = true;
+
+    while (melhorRota)
+    {
+        melhorRota = false;
+        for (int i = 1; i < rotaMelhorada.size() - 2; i++)
+        {
+            for (int j = i + 1; j < rotaMelhorada.size() - 1; j++)
+            {
+                vector<int> novaRota = rotaMelhorada;
+                // Aplicar a troca 2-opt
+                for (int k = i, l = j; k < l; k++, l--)
+                {
+                    swap(novaRota, k, l);
+                }
+
+                double avaliacaoAtual = calculaCustoRota(rotaMelhorada);
+                double novaAvaliacao = calculaCustoRota(novaRota);
+
+                if (novaAvaliacao < avaliacaoAtual)
+                {
+                    rotaMelhorada = novaRota;
+                    melhorRota = true;
+                }
+            }
+        }
+    }
+
+    return rotaMelhorada;
 }
 
 int main()
@@ -77,9 +118,13 @@ int main()
     // TODO: lógica da busca local / meta-heurística
 
     // TESTE CALCULA CUSTO ROTA
-    // vector<int> rota = {0, 3, 4, 5};
-    // int custo = calculaCustoRota(rota);
-    // cout << custo << endl;
+    vector<int> rota = {0, 3, 4, 5, 1, 6, 2};
+    int custoInicial = calculaCustoRota(rota);
+    vector<int> novaRota = vnd(rota);
+    int custoNovo = calculaCustoRota(novaRota);
+
+    cout << "Custo inicial da rota: " << custoInicial << endl;
+    cout << "Custo após 2-opt: " << custoNovo << endl;
 
     return 0;
 }
