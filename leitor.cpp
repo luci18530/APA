fc7daa66a1118a2cb9b09a188f323b9d61ef2d93
bigpedrolucas <p.lucasilva@hotmail.com>
2023-10-21 16:32:32 -0300

    # add funcao calculaCustoRota

--- leitor.cpp
+++ leitor.cpp
@@ -4,15 +4,43 @@
 
 using namespace std;
 
-int main() {
-    // Declaração de variáveis
-    int totalClientes, totalVeiculos, capacidadeVeiculo, entregasMinimas, custoVeiculo;
-    vector<int> demandaClientes, custoTerceirizacao;
-    vector<vector<int>> custoRota;
+// Declaração de variáveis
+int totalClientes, totalVeiculos, capacidadeVeiculo, entregasMinimas, custoVeiculo;
+vector<int> demandaClientes, custoTerceirizacao;
+vector<vector<int>> custoRotas;
 
+int calculaCustoRota(vector<int> &rota)
+{
+    // CUSTO GLOBAL -> SUM(custo_rota) + custo_caminhoes + custo_terceirizacoes
+    int custo = 0;
+    int deposito = rota[0];
+    int ultimo_destino = rota.back();
+
+    for (int i = 0; i < rota.size() - 1; i++)
+    {
+        int origem = rota[i];
+        int destino = rota[i + 1];
+        custo += custoRotas[origem][destino];
+    }
+
+    custo += custoRotas[ultimo_destino][deposito];
+
+    return custo;
+}
+
+void swap(vector<int> &rota, int i, int j)
+{
+    int aux = rota[i];
+    rota[i] = rota[j];
+    rota[j] = aux;
+}
+
+int main()
+{
     // Abrindo o arquivo para leitura
     ifstream arquivoEntrada("entrada.txt");
-    if (!arquivoEntrada.is_open()) {
+    if (!arquivoEntrada.is_open())
+    {
         cerr << "Erro ao abrir o arquivo!" << endl;
         return 1;
     }
@@ -22,27 +50,36 @@ int main() {
 
     // Leitura da demanda dos clientes
     demandaClientes.resize(totalClientes);
-    for (int i = 0; i < totalClientes; i++) {
+    for (int i = 0; i < totalClientes; i++)
+    {
         arquivoEntrada >> demandaClientes[i];
     }
 
     // Leitura do custo de terceirização para cada cliente
     custoTerceirizacao.resize(totalClientes);
-    for (int i = 0; i < totalClientes; i++) {
+    for (int i = 0; i < totalClientes; i++)
+    {
         arquivoEntrada >> custoTerceirizacao[i];
     }
 
     // Leitura da matriz de custo de rota
-    custoRota.resize(totalClientes + 1, vector<int>(totalClientes + 1));
-    for (int i = 0; i <= totalClientes; i++) {
-        for (int j = 0; j <= totalClientes; j++) {
-            arquivoEntrada >> custoRota[i][j];
+    custoRotas.resize(totalClientes + 1, vector<int>(totalClientes + 1));
+    for (int i = 0; i <= totalClientes; i++)
+    {
+        for (int j = 0; j <= totalClientes; j++)
+        {
+            arquivoEntrada >> custoRotas[i][j];
         }
     }
 
     arquivoEntrada.close();
 
     // TODO: lógica da busca local / meta-heurística
+
+    // TESTE CALCULA CUSTO ROTA
+    // vector<int> rota = {0, 3, 4, 5};
+    // int custo = calculaCustoRota(rota);
+    // cout << custo << endl;
 
     return 0;
 }
